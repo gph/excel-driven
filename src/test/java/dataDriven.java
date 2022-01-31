e95de39b0fa2384d067bca9730b7836f7c2fce7a
gph <gph1990@live.com>
2022-01-31 10:21:27 -0300

    data types

--- src/test/java/dataDriven.java
+++ src/test/java/dataDriven.java
@@ -1,23 +1,38 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
 
 public class dataDriven {
 	public static void main(String[] args) throws IOException {
+		dataDriven d = new dataDriven();
+		d.getData("test", "purchase").forEach((cell) -> {
+			System.out.println(cell);
+		});
+
+		ArrayList data = d.getData("test", "purchase");
+		System.out.println(data.get(0));
 		
+	}
+	
+	public ArrayList<String> getData(String sheetName, String columnName) throws IOException {
 		FileInputStream file = new FileInputStream("restapicourse.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
+		
+		ArrayList<String> array = new ArrayList<String>();
 		
 		int sheets = workbook.getNumberOfSheets();
 		System.out.println("Number of sheets: " + sheets);
 		
 		for (int i = 0; i < sheets; i++) {
-			if(workbook.getSheetName(i).equalsIgnoreCase("test"))
+			if(workbook.getSheetName(i).equalsIgnoreCase(sheetName))
 			{
 				XSSFSheet sheet = workbook.getSheetAt(i);
 				Iterator<Row> rows = sheet.iterator();
@@ -25,20 +40,44 @@ public class dataDriven {
 				{
 					Row row = rows.next();
 					Iterator<Cell> cell = row.cellIterator();
+					int k = 0;
+					int column = 0;
+					
 					while(cell.hasNext())
 					{
 						Cell value = cell.next();
-						if(value.toString().equalsIgnoreCase("Testcases")) // search some text in a cell
-						{}
-						// print like a sheet on console
-						System.out.print(value.toString());
-						for (int j = 0; j < (10 - value.toString().length()); j++) {
-							System.out.print(" ");
+						
+						if(value.getStringCellValue().equalsIgnoreCase("testcases")) // search some text in a cell
+						{
+							column = k;
 						}
+						k++;
 					}
 					System.out.println();
+					
+					while(rows.hasNext()) 
+					{
+						Row r = rows.next();
+						if(r.getCell(column).getStringCellValue().toString().equalsIgnoreCase(columnName))
+						{
+							Iterator<Cell> cv = r.cellIterator();
+							while(cv.hasNext())
+							{
+									Cell c = cv.next();
+									if(c.getCellType().equals(CellType.STRING))
+									{
+										array.add(c.getStringCellValue());
+									}
+									else if(c.getCellType().equals(CellType.NUMERIC))
+									{
+										array.add(c.getNumericCellValue()+"");
+									}
+							}
+						}
+					}
 				}
 			}
 		}
+		return array;
 	}
 }
