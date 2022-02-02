b582824232dfbccbef8c277304eb8e85737c9673
gph <gph1990@live.com>
2022-02-02 01:01:43 -0300

    making an actual test with testng

--- src/test/java/DataDriven.java
+++ src/test/java/DataDriven.java
@@ -1,79 +1,99 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class DataDriven {
 	public static void main(String[] args) throws IOException {
-		DataDriven d = new DataDriven();
-		d.getData("test", "testcases", "book1").forEach((cell) -> {
-			System.out.println(cell);
-		});
-
-		// Example 2
-		//ArrayList data = d.getData("test", "case1");
-		//System.out.println(data.get(0));
-		
+		/*
+		 * Local Test :P DataDriven d = new DataDriven(); d.getData("test",
+		 * "testcases");
+		 * 
+		 * ArrayList<Map<String, Object>> data = d.getData("test", "testcases");
+		 * data.forEach(obj -> { System.out.println(obj.get("name")); });
+		 */
 	}
-	
-	public ArrayList<String> getData(String sheetName, String columnName, String cellName) throws IOException {
+
+	@DataProvider(name = "data-books")
+	@Test(dataProvider = "data-sheet")
+	public ArrayList<Map<String, Object>> getData(String sheetName, String columnName) throws IOException {
 		FileInputStream file = new FileInputStream("restapicourse.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
-		
-		ArrayList<String> array = new ArrayList<String>();
-		
+
+		ArrayList<Map<String, Object>> array = new ArrayList<Map<String, Object>>();
+
 		int sheets = workbook.getNumberOfSheets();
 		System.out.println("Number of sheets: " + sheets);
-		
+
 		for (int i = 0; i < sheets; i++) {
-			if(workbook.getSheetName(i).equalsIgnoreCase(sheetName))
-			{
+			if (workbook.getSheetName(i).equalsIgnoreCase(sheetName)) {
 				XSSFSheet sheet = workbook.getSheetAt(i);
 				Iterator<Row> rows = sheet.iterator();
-				while(rows.hasNext())
-				{
+				while (rows.hasNext()) {
 					Row row = rows.next();
 					Iterator<Cell> cell = row.cellIterator();
 					int k = 0;
 					int column = 0;
-					
-					while(cell.hasNext())
-					{
+
+					while (cell.hasNext()) {
 						Cell value = cell.next();
-						
-						if(value.getStringCellValue().equalsIgnoreCase(columnName)) // search some text in a cell
+
+						if (value.getStringCellValue().equalsIgnoreCase(columnName)) // search some text in a cell
 						{
 							column = k;
 						}
 						k++;
 					}
-					System.out.println();
-					
-					while(rows.hasNext()) 
-					{
+
+					while (rows.hasNext()) {
 						Row r = rows.next();
-						if(r.getCell(column).getStringCellValue().toString().equalsIgnoreCase(cellName))
-						{
-							Iterator<Cell> cv = r.cellIterator();
-							while(cv.hasNext())
-							{
-									Cell c = cv.next();
-									if(c.getCellType().equals(CellType.STRING))
-									{
-										array.add(c.getStringCellValue());
-									}
-									else if(c.getCellType().equals(CellType.NUMERIC))
-									{
-										array.add(c.getNumericCellValue()+"");
-									}
+						Map<String, Object> obj = new HashMap<>();
+						Iterator<Cell> cv = r.cellIterator();
+						while (cv.hasNext()) {
+							Cell c = cv.next();
+							if (c.getCellType().equals(CellType.STRING)) {
+								switch (c.getColumnIndex()) {
+								case 1:
+									obj.put("name", c.getStringCellValue());
+									break;
+								case 2:
+									obj.put("isbn", c.getStringCellValue());
+									break;
+								case 3:
+									obj.put("aisle", c.getStringCellValue());
+									break;
+								case 4:
+									obj.put("author", c.getStringCellValue());
+									break;
+								}
+							} else if (c.getCellType().equals(CellType.NUMERIC)) {
+								switch (c.getColumnIndex()) {
+								case 1:
+									obj.put("name", c.getNumericCellValue());
+									break;
+								case 2:
+									obj.put("isbn", c.getNumericCellValue());
+									break;
+								case 3:
+									obj.put("aisle", c.getNumericCellValue());
+									break;
+								case 4:
+									obj.put("author", c.getNumericCellValue());
+									break;
+								}
 							}
 						}
+						array.add(obj);
 					}
 				}
 			}
--- src/test/java/ExcelDriven.java
+++ src/test/java/ExcelDriven.java
@@ -1,10 +1,18 @@
 import static io.restassured.RestAssured.given;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -12,50 +20,43 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class ExcelDriven {
-	@Test
-	public void addBook() throws IOException {
-		RestAssured.baseURI = "http://216.10.245.166";
-		
-		DataDriven d = new DataDriven();
-		ArrayList data = d.getData("test", "testcases", "book1");
-		
-		
-		Map<String, Object> book = new HashMap<>();
-		book.put("name", data.get(1));
-		book.put("isbn", data.get(2));
-		book.put("aisle", data.get(3));
-		book.put("author", data.get(4));
-		
-		// remember> for nested obj create another hashmap
 
-		
-		Response res = given().header("Content-Type", "application/json")
-				.body(book)
-				.when()
-				.post("/Library/Addbook.php")
-				.then().assertThat().statusCode(200).extract().response();
-				
-		JsonPath js = new JsonPath(res.asString());
-		
-		System.out.println("ID: " + js.get("ID"));
-		
-		//deleteBook(js.get("ID"));
-	}
-	
-	public void deleteBook(String id) {
+	@Test(dataProvider = "books")
+	public void addBook(Map<String, Object> book) throws IOException {
 		RestAssured.baseURI = "http://216.10.245.166";
 		
+		Response res = given().header("Content-Type", "application/json").body(book).when().log().all()
+				.post("/Library/Addbook.php").then().log().all().assertThat().statusCode(200).extract().response();
+
+		JsonPath js = new JsonPath(res.asString());
+
+		System.out.println("ID: " + js.get("ID"));
+
+		// deleteBook(js.get("ID"));
+	}
+
+	public void deleteBook(String id) {
+		RestAssured.baseURI = "http://216.10.245.1661";
+
 		Map<String, Object> book = new HashMap<>();
 		book.put("ID", id);
-		
-		Response res = given().header("Content-Type", "application/json")
-		.body(book)
-		.when()
-		.delete("/Library/DeleteBook.php")
-		.then().assertThat().statusCode(200).extract().response();
-		
+
+		Response res = given().header("Content-Type", "application/json").body(book).when()
+				.delete("/Library/DeleteBook.php").then().assertThat().statusCode(200).extract().response();
+
 		JsonPath js = new JsonPath(res.asString());
-		
+
 		System.out.println(js.get("msg").toString());
+	}
+	@DataProvider(name="books")
+	public Object[][] dataProvider() throws IOException {
+		DataDriven data = new DataDriven();
+		ArrayList<Map<String, Object>> books = data.getData("test", "testcases");
+		Object[][] parser = new Object[0][0];
+
+		int i = 0;
+		books.forEach(book -> {
+		});
+		return new Object[][] {{books.get(0)}, {books.get(1)}};
 	}
 }
